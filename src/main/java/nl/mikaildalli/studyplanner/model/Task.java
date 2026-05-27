7be546649262dc77e68769e41f63d8cb122a7c4f
Scheelman <mikail_dalli14@hotmail.com>
2026-05-27 15:33:58 +0200

    Make Task inherit from PlannerItem

--- src/main/java/nl/mikaildalli/studyplanner/model/Task.java
+++ src/main/java/nl/mikaildalli/studyplanner/model/Task.java
@@ -5,24 +5,19 @@ import java.time.LocalDate;
 // Task Model:
 // ID is nodig voor UPDATE/DELETE
 // namen komen uit JOIN voor TableView
-public class Task {
-    private final int id;
-    private final String title;
-    private final String description;
-    private final LocalDate deadline;
+public class Task extends PlannerItem {
 
+    private final LocalDate deadline;
     private final int statusId;
     private final Integer categoryId;
-
     private final String statusName;
     private final String categoryName;
 
     public Task(int id, String title, String description, LocalDate deadline,
-                int statusId, Integer categoryId,
-                String statusName, String categoryName) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
+                int statusId, Integer categoryId, String statusName, String categoryName) {
+
+        super(id, title, description);
+
         this.deadline = deadline;
         this.statusId = statusId;
         this.categoryId = categoryId;
@@ -30,14 +25,27 @@ public class Task {
         this.categoryName = categoryName;
     }
 
-    public int getId() { return id; }
-    public String getTitle() { return title; }
-    public String getDescription() { return description; }
-    public LocalDate getDeadline() { return deadline; }
+    public LocalDate getDeadline() {
+        return deadline;
+    }
 
-    public int getStatusId() { return statusId; }
-    public Integer getCategoryId() { return categoryId; }
+    public int getStatusId() {
+        return statusId;
+    }
+    public Integer getCategoryId() {
+        return categoryId;
+    }
 
-    public String getStatusName() { return statusName; }
-    public String getCategoryName() { return categoryName; }
+    public String getStatusName() {
+        return statusName;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+@Override
+    public  String getType() {
+        return "Task";
+}
 }
